4132e045a1ee5064cfccd2b5928804ac681364d6
Kosuke Shinto <kou.shinto@icloud.com>
2022-08-28 01:00:06 +0900

    refactor index.php

--- themes/wp-rounded/index.php
+++ themes/wp-rounded/index.php
@@ -1,20 +1,6 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <script src="<?php echo get_template_directory_uri(); ?>/dist/bundle.js"></script>
-</head>
-<body>
+<?php get_header(); ?>
 
-<header>
-header
-</header>
-<!-- 
-  投稿
- -->
+<!-- 投稿 -->
 <?php if( have_posts() ): while( have_posts() ): the_post(); ?>
   <ul class="title">
     <li>
@@ -22,5 +8,5 @@ header
     </li>
   </ul>
 <?php endwhile; endif; ?>
-</body>
-</html>
+
+<?php get_footer(); ?>
---
+++ themes/wp-rounded/single.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+
+The Single
+
+<?php get_footer(); ?>
